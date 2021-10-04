663798b00f84d262eca0f0ef437e4f94227764cd
tjdanqkr <tjdanqkr@gmail.com>
2021-10-04 20:39:43 +0900

    validate

--- server/tweets/tweets.js
+++ server/tweets/tweets.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import 'express-async-error';
+import { body } from 'express-validator';
 import {
   createTweets,
   getAllTweets,
@@ -7,15 +8,21 @@ import {
   deleteTweet,
   updateTweet,
 } from '../controller/tweetsController.js';
+import { validate } from '../middleware/valudate.js';
 const router = express.Router();
 
 router.get('/', getAllTweets);
 
-router.post('/', createTweets);
+const validateTweet = [
+  body('text').trim().isLength({ min: 3 }).withMessage('3글자 이상'),
+  validate,
+];
+
+router.post('/', validateTweet, createTweets);
 
 router.delete('/:id', deleteTweet);
 
-router.put('/:id', updateTweet);
+router.put('/:id', validateTweet, updateTweet);
 
 router.get('/:id', getTweetsById);
 
